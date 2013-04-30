dee03c57a6372d7d53f15f6aca5fd9e287375101
JR Mallon <JRMallon@gmail.com>
2013-04-29 20:34:45 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Disclosio::Application.config.secret_token = '533546f9a70621356e1d9db23f4dee01f8cb8621e0253f4aa5afc8ea52453ba66a9d95f27b9085d665eb0fd5bb0cc3d7ee858004f4df157c069d894327c0cf8e'
