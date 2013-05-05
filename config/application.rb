b4728c18fa3064b0c805a778396e36e7a8c4ce07
JR Mallon <JRMallon@gmail.com>
2013-05-05 09:12:12 -0700

    Add user model with Devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Disclosio
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Required by devise on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
